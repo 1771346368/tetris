2077ff36f272be357ddcd0248cc3f77e1d96b49e
dairui <1771346368@qq.com>
2024-03-03 22:14:46 +0800

    feat: 增加样式初始化normalize.css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './normalize.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // 严格模式
   <React.StrictMode>
     <App />
   </React.StrictMode>
