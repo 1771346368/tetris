342ebf89281b1458f27bd9e0a83583bf7ede68ad
dairui <1771346368@qq.com>
2024-03-03 22:13:43 +0800

    feat: 利用craco调整webpack配置，用以支持less与css modules

---
+++ craco.config.js
@@ -0,0 +1,19 @@
+const CracoCSSModules = require('craco-css-modules');
+const CracoLessPlugin = require('craco-less');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {},
+            javascriptEnabled: true
+          }
+        }
+      }
+    },
+    { plugin: CracoCSSModules }
+  ]
+};
